830a3b578f7394c3cc48eb503399a51172318842
Eduardo Rohde Eras <eduardo.eras@inpe.br>
2026-05-15 19:22:30 +0000

    Model ready to run after added new 'stability_flag' variable to Registry.xml and new variables to stream output list as '.TAYLOR'.

--- make-all.sh
+++ make-all.sh
@@ -30,14 +30,31 @@
 #    PRECISION=single - builds with default single-precision real kind. Default is to use double-precision.
 #    SHAREDLIB=true - generate position-independent code suitable for use in a shared library. Default is false.
 
-cd /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/scripts
-. /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/scripts/setenv.bash
-cd /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+MONAN_ROOT="$(cd "${SCRIPT_DIR}/../.." && pwd)"
+SCRIPTS_DIR="${MONAN_ROOT}/scripts"
+SOURCE_DIR="${MONAN_ROOT}/sources/MONAN-Model_1.4.3-rc"
+EXECS_DIR="${MONAN_ROOT}/execs"
 
-rm -rf /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/default_inputs/ /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/src/core_atmosphere/physics/physics_wrf/files
-rm -f  /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/stream_list.* /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/streams.* /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/namelist.* 
-rm -f  /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/make*.output.atmosphere /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/make*.output.init_atmosphere 
-rm -fr /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/src/core_atmosphere/inc /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/src/core_init_atmosphere/inc
+echo "-=+*#@#*@#*+=--=+*#@#*@#*+=--=+*#@#*@#*+=--=+*#@#*@#*+=-"
+echo ""
+echo "Script Directory: ${SCRIPT_DIR}"
+echo "MONAN Root Directory: ${MONAN_ROOT}"
+echo "Scripts Directory: ${SCRIPTS_DIR}"
+echo "Source Directory: ${SOURCE_DIR}"
+echo "Executables Directory: ${EXECS_DIR}"
+echo ""
+echo "-=+*#@#*@#*+=--=+*#@#*@#*+=--=+*#@#*@#*+=--=+*#@#*@#*+=-"
+sleep 4
+
+cd "${SCRIPTS_DIR}"
+. "${SCRIPTS_DIR}/setenv.bash"
+cd "${SOURCE_DIR}"
+
+rm -rf "${SOURCE_DIR}/default_inputs/" "${SOURCE_DIR}/src/core_atmosphere/physics/physics_wrf/files"
+rm -f  "${SOURCE_DIR}/"stream_list.* "${SOURCE_DIR}/"streams.* "${SOURCE_DIR}/"namelist.*
+rm -f  "${SOURCE_DIR}/"make*.output.atmosphere "${SOURCE_DIR}/"make*.output.init_atmosphere
+rm -fr "${SOURCE_DIR}/src/core_atmosphere/inc" "${SOURCE_DIR}/src/core_init_atmosphere/inc"
 DATE_TIME_NOW=$(date +"%Y%m%d%H%M%S")
 
 export NETCDF=${NETCDFDIR}
@@ -56,10 +73,10 @@ make -j 8 intel-xd2000 CORE=atmosphere OPENMP=true USE_PIO2=false PRECISION=sing
 
 
 #CR: TODO: put verify here if executable was created ok
-mv /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/atmosphere_model /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs
-mv /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/build_tables /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs
-cp /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/VERSION.txt /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs/MONAN-VERSION.txt
-cp /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/GF_ConvPar_nml /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/scripts
+mv "${SOURCE_DIR}/atmosphere_model" "${EXECS_DIR}"
+mv "${SOURCE_DIR}/build_tables" "${EXECS_DIR}"
+cp "${SOURCE_DIR}/VERSION.txt" "${EXECS_DIR}/MONAN-VERSION.txt"
+cp "${SOURCE_DIR}/GF_ConvPar_nml" "${SCRIPTS_DIR}"
 make clean CORE=atmosphere
 
 MAKE_OUT_FILE="make_${DATE_TIME_NOW}_.output.init_atmosphere"
@@ -71,13 +88,13 @@ make -j 8 intel2-xd2000 CORE=init_atmosphere OPENMP=true USE_PIO2=false PRECISIO
 #make -j 8 intel-xd2000 CORE=init_atmosphere OPENMP=true USE_PIO2=false PRECISION=single OPTIMIZATION_LEVEL=O1 FFLAGS_OPT=-O1 CFLAGS_OPT=-O1 CXXFLAGS_OPT=-O1 2>&1 | tee ${MAKE_OUT_FILE}
 
 
-mv /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/sources/MONAN-Model_1.4.3-rc/init_atmosphere_model /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs
+mv "${SOURCE_DIR}/init_atmosphere_model" "${EXECS_DIR}"
 make clean CORE=init_atmosphere
 
 
-if [ -s "/p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs/init_atmosphere_model" ] && [ -e "/p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs/atmosphere_model" ]; then
+if [ -s "${EXECS_DIR}/init_atmosphere_model" ] && [ -e "${EXECS_DIR}/atmosphere_model" ]; then
     echo ""
-    echo -e "\033[1;32m==>\033[0m Files init_atmosphere_model and atmosphere_model generated Successfully in /p/projetos/monan_atm/eduardo.eras/MONAN/scripts_CD-CT/execs !"
+    echo -e "\033[1;32m==>\033[0m Files init_atmosphere_model and atmosphere_model generated Successfully in ${EXECS_DIR} !"
     echo
 else
     echo -e "\033[1;31m==>\033[0m !!! An error occurred during build. Check output"
